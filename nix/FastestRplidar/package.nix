4279014819c25fd6a516ccb1ff6e3ee1b8839512
purepani <pani0028@umn.edu>
2023-09-08 15:38:23 -0500

    changed to python 3.9

--- nix/FastestRplidar/package.nix
+++ nix/FastestRplidar/package.nix
@@ -4,11 +4,15 @@
 }: let
   system = "x86_64-linux";
   pkgs = inputs.nixpkgs;
+  mach-nix = import inputs.mach-nix {
+    inherit pkgs;
+    python = "python39";
+  };
 in {
-  default = inputs.mach-nix.lib."${system}".buildPythonPackage {
+  default = mach-nix.buildPythonPackage {
     src = inputs.self;
     nativeBuildInputs = [pkgs.swig4 pkgs.gccMultiStdenv inputs.cells.rplidar_sdk.package];
-    buildInputs = [pkgs.python310];
+    #buildInputs = [pkgs.python310];
     preConfigure = ''
       cp ${inputs.cells.rplidar_sdk.package}/bin/libsl_lidar_sdk.a librplidar_sdk.a
       ${pkgs.swig4}/bin/swig -c++ -python fastestrplidar.i
